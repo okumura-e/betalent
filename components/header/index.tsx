a8c7ad824016dc20877c37aab344a2e5869c2a49
Adriel Itamar <adrielitamar@gmail.com>
2025-02-15 16:49:55 -0300

    refactor: add style file

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text } from 'react-native';
 import Octicons from '@expo/vector-icons/Octicons';
+import styles from './styles';
 
 export default function Header() {
     return (
@@ -17,42 +18,3 @@ export default function Header() {
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingHorizontal: 16,
-        height: 79,
-    },
-    headerLogo: {
-        fontSize: 16,
-        fontWeight: '500',
-        borderRadius: 50,
-        backgroundColor: '#F5F5F5',
-        width: 45,
-        height: 45,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    notificationContainer: {
-        position: 'relative',
-    },
-    badge: {
-        position: 'absolute',
-        right: -4,
-        top: -4,
-        backgroundColor: '#0066FF',
-        borderRadius: 50,
-        width: 20,
-        height: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    badgeText: {
-        color: '#fff',
-        fontSize: 10,
-        fontWeight: '700',
-    },
-});
